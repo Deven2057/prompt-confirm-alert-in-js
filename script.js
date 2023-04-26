fed7ebedb6c6f7243c27f0103e83ee7472aa9d82
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-04-26 16:32:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("hello i am alert")
+
+
+let a=prompt("enter any number")
+a=Number.parseInt(a)
+ 
+
+alert(typeof a)
+
+let write = confirm(" write the number in page?")
+if(write){
+  
+
+document.write(a)
+}
+else{
+document.write("ok no prbl.")
+  
+}
