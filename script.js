d8f82c745513eca7195490780bcb78150aa60d28
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-04-26 16:34:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ else{
 document.write("ok no prbl.")
   
 }
+
+alert("hello i am alert")
+
+
+let a=prompt("enter any number")
+a=Number.parseInt(a)
+ 
+
+alert(typeof a)
+
+let write = confirm(" write the number in page?")
+if(write){
+  
+
+document.write(a)
+}
+else{
+document.write("ok no prbl.")
+  
+}
+
